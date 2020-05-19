91bcb0308d019979f6588c7968ed9b6615db4b2e
Kristof <kristof.kovacs@telenav.com>
2020-05-19 18:02:57 +0300

    new version

---
+++ amazon_affiliate_url/amazon_affiliate_url.py
@@ -0,0 +1,9 @@
+class AmazonAffiliateURL:
+    def __init__(self, affiliate_tag: str):
+        self.affiliate_tag = affiliate_tag
+    
+    def url(self, asin: str) -> str:
+        return url(self.affiliate_tag, asin)
+
+def url(affiliate_tag: str, asin: str) -> str:
+    return 'https://www.amazon.com/gp/product/' + asin + '?tag=' + affiliate_tag
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="amazon_affiliate_url",
+    version="0.0.2",
+    author="Kristof",
+    description="amazon_affiliate_url",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/amazon_affiliate_url",
+    packages=setuptools.find_packages(),
+    install_requires=[""],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
